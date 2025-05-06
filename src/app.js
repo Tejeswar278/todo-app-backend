d2e112f7c5c548594c9e124a2844b7566f663cc0
Tejeswar073 <tejeswararao@pinacalabs.com>
2025-05-06 12:10:47 +0530

    added port to env

--- src/app.js
+++ src/app.js
@@ -19,5 +19,5 @@ const todoRoutes = require("./routes/todo");
 app.use("/api/users", userRoutes);
 app.use("/api/todos", todoRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
